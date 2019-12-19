d3b198f2d0f07abeca3a8bd94873a79e33766523
Zaslavskiy Mike <Mikhail.Zaslavskiy@x5.ru>
2019-12-19 13:52:40 +0300

    indentation issue fixed in network client implementation

--- BookSearch/BookSearch/Data Access/Network/Client/NetworkClientImp.swift
+++ BookSearch/BookSearch/Data Access/Network/Client/NetworkClientImp.swift
@@ -9,77 +9,74 @@
 import Foundation
 
 final class NetworkClientImp {
-  
-  private var session: URLSession = {
-    let configuration = URLSessionConfiguration.default
-    configuration.timeoutIntervalForRequest = 3
-    let session = URLSession(configuration: configuration)
-    return session
-  }()
+    private var session: URLSession = {
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = 3
+        let session = URLSession(configuration: configuration)
+        return session
+    }()
 }
 
 // MARK: - NetworkClient
 
 extension NetworkClientImp: NetworkClient {
-  
-  func request(endpoint: Endpoint, response: @escaping (Result<Data, NetworkError>) -> Void) {
-    
-    guard var components = URLComponents(string: endpoint.fullPath) else {
-      response(.failure(.wrongEndpoint))
-      return
+    func request(endpoint: Endpoint, response: @escaping (Result<Data, NetworkError>) -> Void) {
+        
+        guard var components = URLComponents(string: endpoint.fullPath) else {
+            response(.failure(.wrongEndpoint))
+            return
+        }
+        
+        components.queryItems = endpoint.headers.map { URLQueryItem(name: $0.0, value: $0.1) }
+        
+        guard let url = components.url else {
+            response(.failure(.wrongEndpoint))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        
+        switch endpoint.task {
+        case .request:
+            break
+            
+        case .requestData(let data):
+            request.httpBody = data
+        }
+        
+        
+        let task = session.dataTask(with: request) { data, resp, error in
+            if let error = error {
+                response(.failure(.transport(error.localizedDescription)))
+                return
+            }
+            
+            guard let data = data else {
+                response(.failure(.emptyResponse))
+                return
+            }
+            
+            response(.success(data))
+        }
+        
+        task.resume()
     }
-  
-    components.queryItems = endpoint.headers.map { URLQueryItem(name: $0.0, value: $0.1) }
-    
-    guard let url = components.url else {
-      response(.failure(.wrongEndpoint))
-      return
-    }
-    
-    var request = URLRequest(url: url)
-    
-    switch endpoint.task {
-    case .request:
-      break
-      
-    case .requestData(let data):
-      request.httpBody = data
-    }
-
-    
-    let task = session.dataTask(with: request) { data, resp, error in
-      if let error = error {
-        response(.failure(.transport(error.localizedDescription)))
-        return
-      }
-      
-      guard let data = data else {
-        response(.failure(.emptyResponse))
-        return
-      }
-      
-      response(.success(data))
-    }
-    
-    task.resume()
-  }
 }
 
 // MARK: - Private. Helpers
 
 private extension Endpoint {
-  
-  var fullPath: String {
-    var finalBase = baseUrl
-    if !finalBase.hasSuffix("/") {
-      finalBase += "/"
+    var fullPath: String {
+        var finalBase = baseUrl
+        if !finalBase.hasSuffix("/") {
+            finalBase += "/"
+        }
+        
+        var finalPath = path
+        if !finalPath.hasSuffix("/") {
+            finalPath += "/"
+        }
+        
+        return finalBase + finalPath
     }
-    
-    var finalPath = path
-    if !finalPath.hasSuffix("/") {
-      finalPath += "/"
-    }
-    
-    return finalBase + finalPath
-  }
 }
