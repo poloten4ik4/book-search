233a19cefa78684b304394fce83e87171afa6be7
Zaslavskiy Mike <Mikhail.Zaslavskiy@x5.ru>
2019-12-20 16:11:29 +0300

    Removed unnecessary folder from project

--- BookSearch/BookSearch/Common/AppDelegate.swift
+++ BookSearch/BookSearch/Common/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         // Window setup
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
